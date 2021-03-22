16e5a57dc9840f216d7529b42b9ac5cf6d8946cc
chandan0-1 <chandanofficial.cn@gmail.com>
2021-03-22 20:39:01 +0530

    Added Adding data functionality

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const app = express();
 const path = require("path");
 
 app.set('view engine',"ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded());//Parser 
+app.use(express.static("assest"));
 
 
 var items = [{
   "name" : "Chandan Kumar",
   "phone": "8984703151"
-}];
+  },{
+    name:"Prince",
+    phone:"9638521470"
+  },
+  {
+    name:"Rohit",
+    phone:"6371276091"
+  }
+
+];
 
 app.get("/",function (req,res){
   return res.render("contact_list",{
@@ -19,7 +31,15 @@ app.get("/",function (req,res){
 
 // Accepting Records from From 
 app.post("/contact_path",function(req,res){
-  return res.redirect("/")//Need to give page location
+
+  items.push({
+    name:req.body.my_name,
+    phone:req.body.my_phone
+  })
+  // ------------OR----------
+  // items.push(req.body); here keyname should be same
+
+  return res.redirect("/");//Need to give page location
 })
 
 
