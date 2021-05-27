8ccf66996d5339a5731e10a720e253ffdf1b1da3
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-27 20:40:10 +0530

    Ready for deployment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const db = require("./config/mongoose");
 const app = express();
 const path = require("path");
